3fe6c82cca1b0e6c4e1181d8d03a156384f012c4
ender-zhao <77391690+ender-zhao@users.noreply.github.com>
2022-12-03 12:06:16 +0800

    Update eg-ads.js

--- Customize/eg-ads.js
+++ Customize/eg-ads.js
@@ -2,11 +2,11 @@
   "feedback": [
     {
       "img": "https://raw.githubusercontent.com/ender-zhao/CFW-custom-made/main/Customize/eg-picture-ads.png",
-      "click": "https://github.com/ender-zhao/CFW-custom-made"
+      "click": "https://github.com/ender-zhao/CFW-custom-made#%E5%B9%BF%E5%91%8A"
     },
     {
       "img": "https://raw.githubusercontent.com/ender-zhao/CFW-custom-made/main/Customize/eg-picture-ads.png",
-      "click": "https://github.com/ender-zhao/CFW-custom-made"
+      "click": "https://github.com/ender-zhao/CFW-custom-made#%E5%B9%BF%E5%91%8A"
     }
   ]
 }
