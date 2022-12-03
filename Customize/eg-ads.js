01d1876c5671492f76abd7fd6b57b9feb98a279f
ender-zhao <77391690+ender-zhao@users.noreply.github.com>
2022-12-03 10:42:20 +0800

    Create eg-ads.js

---
+++ Customize/eg-ads.js
@@ -0,0 +1,12 @@
+{
+  "feedback": [
+    {
+      "img": "https://tly33.com/asset/clash.gif",
+      "click": "https://tly.llc"
+    },
+    {
+      "img": "https://jindouyun.pao1.site/ad_jike.png",
+      "click": "https://jike0001.com/auth/register?code=gcYV"
+    }
+  ]
+}
